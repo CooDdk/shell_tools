190516995f55aa14272e99809c6681e5b652c590
CooDdk <695438516@qq.com>
2020-08-04 21:58:58 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -10,9 +10,7 @@ tar_file_prefix=`date -d"-1 month days" +%Y_%m`
 tar_file=${tar_file_prefix}.log.tar.gz
 source_file=*${last_month}_*.log
 echo ${source_file}
-cd /opt/www/test1/ && tar -zcvf ${tar_file} ${source_file} && rm -rf ${source_file}
-
-#cd /opt/www/test2/
-#tar -zcvf $tar_file *$last_month_*.log && rm -rf *$last_month__*.log
+log_dir=/opt/www/test1/
+cd ${log_dir} && tar -zcvf ${tar_file} ${source_file} && rm -rf ${source_file}
 
 echo '备份完成'
